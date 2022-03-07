dbba056f05a5751051bed411bcb2b06c49476373
Patryk Marczak <m.patryk@outlook.com>
2022-03-07 19:30:47 +0100

    added moving rectangle

--- src/main.js
+++ src/main.js
@@ -1,18 +1,24 @@
 import Phaser from 'phaser'
-
 import HelloWorldScene from './scenes/HelloWorldScene'
+import axios from 'axios'
 
 const config = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	width: window.innerWidth,
+	height: window.innerHeight,
 	physics: {
 		default: 'arcade',
 		arcade: {
 			gravity: { y: 200 }
 		}
 	},
+	scale: { mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH },
 	scene: [HelloWorldScene]
 }
 
-export default new Phaser.Game(config)
+axios.post('http://localhost:8081/create', { 'gui': 'ArrowsHorizontal', 'max_players': 2 })
+	.then(res => console.log(res)).catch(error => console.log(error))
+
+const game = new Phaser.Game(config)
+
+export default game;
--- src/scenes/HelloWorldScene.js
+++ src/scenes/HelloWorldScene.js
@@ -1,14 +1,11 @@
 import Phaser from 'phaser'
 
-export default class HelloWorldScene extends Phaser.Scene
-{
-	constructor()
-	{
-		super('hello-world')
-	}
+export default class HelloWorldScene extends Phaser.Scene {
+    constructor() {
+        super('hello-world')
+    }
 
-	preload()
-    {
+    preload() {
         this.load.setBaseURL('http://labs.phaser.io')
 
         this.load.image('sky', 'assets/skies/space3.png')
@@ -16,24 +13,28 @@ export default class HelloWorldScene extends Phaser.Scene
         this.load.image('red', 'assets/particles/red.png')
     }
 
-    create()
-    {
-        this.add.image(400, 300, 'sky')
+    create() {
+        this.rect = this.add.rectangle(100, 100, 200, 300, 0x6666ff)
+        console.log(this.physics.world.bounds)
 
-        const particles = this.add.particles('red')
+        this.key_d = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+        this.key_a = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
+        this.key_w = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
+        this.key_s = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
+    }
 
-        const emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        })
-
-        const logo = this.physics.add.image(400, 100, 'logo')
-
-        logo.setVelocity(100, 200)
-        logo.setBounce(1, 1)
-        logo.setCollideWorldBounds(true)
-
-        emitter.startFollow(logo)
+    update(time, delta) {
+        if (this.key_d.isDown && this.rect.x + this.rect.width / 2 < this.physics.world.bounds.right) {
+            this.rect.x += delta * 1;
+        }
+        if (this.key_a.isDown && this.rect.x - this.rect.width / 2 > 0) {
+            this.rect.x -= delta * 1;
+        }
+        if (this.key_w.isDown && this.rect.y - this.rect.height / 2 > 0) {
+            this.rect.y -= delta * 1;
+        }
+        if (this.key_s.isDown && this.rect.y + this.rect.height / 2 < this.physics.world.bounds.bottom) {
+            this.rect.y += delta * 1;
+        }
     }
 }
